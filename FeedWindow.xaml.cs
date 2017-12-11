9fa1c0f4b6dbede582a94d2ccbb82b665282f466
AG235HB <AG235HB@hotmail.com>
2017-12-11 16:32:04 +0300

    rss feeds can be stored in config

--- FeedWindow.xaml.cs
+++ FeedWindow.xaml.cs
@@ -19,9 +19,63 @@ namespace RSS_reader
     /// </summary>
     public partial class FeedWindow : Window
     {
-        public FeedWindow()
+        FeedSettings fSettings = new FeedSettings();
+        public FeedWindow(ref FeedSettings settings)
         {
             InitializeComponent();
+
+            fSettings = settings;
+            FillList();
+        }
+
+        private void FillList()
+        {
+            foreach (FeedItem fi in fSettings.Items)
+            {
+                listView.Items.Add(fi._name);
+            }
+        }
+
+        private void btn_Add_Click(object sender, RoutedEventArgs e)
+        {
+            label0.IsEnabled = true;
+            label1.IsEnabled = true;
+            tb_FeedLink.IsEnabled = true;
+            tb_FeedName.IsEnabled = true;
+            btn_AddFeed.IsEnabled = true;
+            btn_Abort.IsEnabled = true;
+        }
+
+        private void btn_AddFeed_Click(object sender, RoutedEventArgs e)
+        {
+            FeedItem fi = new FeedItem(tb_FeedName.Text, tb_FeedLink.Text);
+            fSettings.Items.Add(fi);
+            listView.Items.Clear();
+            FillList();
+
+            label0.IsEnabled = false;
+            label1.IsEnabled = false;
+            tb_FeedLink.IsEnabled = false;
+            tb_FeedName.IsEnabled = false;
+            btn_AddFeed.IsEnabled = false;
+            btn_Abort.IsEnabled = false;
+        }
+
+        private void btn_Abort_Click(object sender, RoutedEventArgs e)
+        {
+            label0.IsEnabled = false;
+            label1.IsEnabled = false;
+            tb_FeedLink.IsEnabled = false;
+            tb_FeedName.IsEnabled = false;
+            btn_AddFeed.IsEnabled = false;
+            btn_Abort.IsEnabled = false;
+        }
+
+        private void btn_Del_Click(object sender, RoutedEventArgs e)
+        {
+            fSettings.Items.Remove(fSettings.Items[listView.SelectedIndex]);
+            listView.Items.Clear();
+            FillList();
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,18 +2,16 @@
 using System.Data.Entity;
 using System.IO;
 using System.Windows;
-using System.Runtime.Serialization;
-//using System.Runtime.Serialization.Json;
-//using System.Collections.Generic;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace RSS_reader
 {
-    class FeedSettings
+    public class FeedSettings
     {
-        public FeedItem[] Items { get; set; }
+        public List<FeedItem> Items { get; set; }
     }
-    class FeedItem
+    public class FeedItem
     {
         public string _name { get; set; }
         public string _link { get; set; }
@@ -33,6 +31,7 @@ namespace RSS_reader
         ApplicationContext db;
 
         RssFeed currentFeed;
+        public FeedSettings fSettings = new FeedSettings();
 
         public MainWindow()
         {
@@ -42,25 +41,24 @@ namespace RSS_reader
             db.Posts.Load();
             this.DataContext = db.Posts.Local.ToBindingList();
 
-            FeedSettings settings = new FeedSettings();
-            settings.Items = new FeedItem[2];
-            settings.Items[0] = new FeedItem("UNO", "uno");
-            settings.Items[1] = new FeedItem("DOS", "dos");
-            using (FileStream fs = new FileStream("feedsettings.json", FileMode.OpenOrCreate))
-            {
-                string serialized = JsonConvert.SerializeObject(settings);
-                //fs.
-                MessageBox.Show(serialized);
-            }
-
             using (FileStream fs = new FileStream("feedsettings.json", FileMode.Open))
             {
                 byte[] bArray = new byte[fs.Length];
                 fs.Read(bArray, 0, bArray.Length);
                 string tmp = System.Text.Encoding.Default.GetString(bArray);
-                FeedSettings fSettings = JsonConvert.DeserializeObject<FeedSettings>(tmp);
-                foreach (FeedItem fi in fSettings.Items)
-                    MessageBox.Show("Name: " + fi._name + " Link: " + fi._link);
+                fSettings = JsonConvert.DeserializeObject<FeedSettings>(tmp);
+                FillComboBox();
+            }
+
+            
+        }
+
+        private void FillComboBox()
+        {
+            foreach (FeedItem fi in fSettings.Items)
+            {
+                //MessageBox.Show("Name: " + fi._name + " Link: " + fi._link);
+                chBox_RSS.Items.Add(fi._name);
             }
         }
 
@@ -68,22 +66,18 @@ namespace RSS_reader
         {
             try
             {
-                if (!String.IsNullOrEmpty(URL_textBox.Text))
-                {
-                    currentFeed = new RssFeed(URL_textBox.Text);
-                    if (currentFeed.items.Count > 0)
-                        foreach (RssItem feedItem in currentFeed.items)
-                        {
-                            Post _post = new Post();
-                            _post.Title = feedItem.title;
-                            _post.Description = feedItem.description;
-                            _post.Link = feedItem.link;
+                currentFeed = new RssFeed(fSettings.Items[chBox_RSS.SelectedIndex]._link);
+                if (currentFeed.items.Count > 0)
+                    foreach (RssItem feedItem in currentFeed.items)
+                    {
+                        Post _post = new Post();
+                        _post.Title = feedItem.title;
+                        _post.Description = feedItem.description;
+                        _post.Link = feedItem.link;
 
-                            db.Posts.Add(_post);
-                            db.SaveChanges();
-                        }
-                    else MessageBox.Show("Feed is empty.");
-                }
+                        db.Posts.Add(_post);
+                        db.SaveChanges();
+                    }
             }
             catch(Exception ex)
             { MessageBox.Show(ex.Message); }
@@ -91,10 +85,29 @@ namespace RSS_reader
 
         private void postList_MouseDoubleClick_1(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            //MessageBox.Show("CLICK");
             Post _post = postList.SelectedItem as Post;
             PostWindow _pWindow = new PostWindow(_post);
             _pWindow.ShowDialog();
+        }
+
+        private void bFeedSettings_Click(object sender, RoutedEventArgs e)
+        {
+            FeedWindow fWindow = new FeedWindow(ref fSettings);
+            fWindow.ShowDialog();
+            //if (fWindow.ShowDialog() == true)
+            
+        }
+
+        private void btn_Refresh_Click(object sender, RoutedEventArgs e)
+        {
+            chBox_RSS.Items.Clear();
+            FillComboBox();
+            using (FileStream fs = new FileStream("feedsettings.json", FileMode.Truncate))
+            {
+                string tmp = JsonConvert.SerializeObject(fSettings);
+                byte[] bArray = System.Text.Encoding.Default.GetBytes(tmp);
+                fs.Write(bArray, 0, bArray.Length);
+            }
         }
     }
 }
